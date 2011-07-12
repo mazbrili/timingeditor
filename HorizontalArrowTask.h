7bb47f804aab2acb4ccbff8a2e6fa9ae950f4d02
d_anselmi <d_anselmi@68a61d50-3e43-0410-840a-95688c06bdb3>
2011-07-12 22:43:53 +0000

    Horizontal Arrow tasks added

---
+++ HorizontalArrowTask.h
@@ -0,0 +1,17 @@
+#ifndef HORIZONTALARROWTASK_H
+#define HORIZONTALARROWTASK_H
+
+#include "Task.h"
+
+
+class HorizontalArrowTask : public Task
+{
+public:
+    HorizontalArrowTask(const Task *task);
+    virtual ~HorizontalArrowTask();
+    virtual void Update()=0;
+protected:
+protected:
+};
+
+#endif // HORIZONTALARROWTASK_H
