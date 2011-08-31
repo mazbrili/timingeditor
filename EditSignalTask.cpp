b04aad398e7c30ad45c456c586e60d66d070565d
d_anselmi <d_anselmi@68a61d50-3e43-0410-840a-95688c06bdb3>
2011-08-31 17:53:13 +0000

    defaults for EditSignalTask

--- EditSignalTask.cpp
+++ EditSignalTask.cpp
@@ -14,3 +14,46 @@ EditSignalTask::~EditSignalTask()
 {
     //dtor
 }
+
+void EditSignalTask::WavesMouse(const wxMouseEvent &event, const wxPoint &pos)
+{
+
+}
+
+
+void EditSignalTask::SetDrawlets()
+{
+
+}
+
+
+void EditSignalTask::OnMouse(const wxMouseEvent &event)
+{
+    if (event.ButtonDown(wxMOUSE_BTN_ANY ))
+        EndTask();
+}
+void EditSignalTask::OnKey(const wxKeyEvent &event, bool down)
+{
+    if ( down && event.GetKeyCode() == WXK_ESCAPE)
+        EndTask();
+}
+void EditSignalTask::LabelsMouse(const wxMouseEvent &event, const wxPoint &pos)
+{
+    OnMouse(event);
+}
+void EditSignalTask::AxisMouse(const wxMouseEvent &event, const wxPoint &pos)
+{
+    OnMouse(event);
+}
+void EditSignalTask::LabelsKey(const wxKeyEvent &event, bool down)
+{
+    OnKey(event, down);
+}
+void EditSignalTask::WavesKey(const wxKeyEvent &event, bool down)
+{
+    OnKey(event, down);
+}
+void EditSignalTask::AxisKey(const wxKeyEvent &event, bool down)
+{
+    OnKey(event, down);
+}
--- EditSignalTask.h
+++ EditSignalTask.h
@@ -9,7 +9,19 @@ class EditSignalTask : public Task
 public:
     EditSignalTask(const Task *task, unsigned int sig, wxInt32 tick, bool leftDown = true);
     virtual ~EditSignalTask();
+
+    virtual void LabelsMouse(const wxMouseEvent &event, const wxPoint &pos);
+    virtual void WavesMouse(const wxMouseEvent &event, const wxPoint &pos);
+    virtual void AxisMouse(const wxMouseEvent &event, const wxPoint &pos);
+    virtual void LabelsKey(const wxKeyEvent &event, bool down);
+    virtual void WavesKey(const wxKeyEvent &event, bool down);
+    virtual void AxisKey(const wxKeyEvent &event, bool down);
+
 protected:
+private:
+    void SetDrawlets();
+    void OnMouse(const wxMouseEvent &event);
+    void OnKey(const wxKeyEvent &event, bool down);
 private:
     unsigned int m_sig;
     bool m_leftDown;
